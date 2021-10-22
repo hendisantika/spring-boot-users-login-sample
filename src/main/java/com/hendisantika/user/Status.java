2f6306f0085fa82ccf01c22a385619bfa50ff57f
Hendi Santika <hendisantika@gmail.com>
2021-10-22 09:05:09 +0700

    #4 - Add Status Enum

---
+++ src/main/java/com/hendisantika/user/Status.java
@@ -0,0 +1,16 @@
+package com.hendisantika.user;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-users-login-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 22/10/21
+ * Time: 09.04
+ */
+public enum Status {
+    SUCCESS,
+    USER_ALREADY_EXISTS,
+    FAILURE
+}
