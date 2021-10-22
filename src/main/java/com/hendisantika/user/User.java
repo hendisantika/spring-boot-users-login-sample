797bbd93139b6c39df017c04f22119b459df418e
Hendi Santika <hendisantika@gmail.com>
2021-10-22 09:07:18 +0700

    #5 - Add User Domain

---
+++ src/main/java/com/hendisantika/user/User.java
@@ -0,0 +1,35 @@
+package com.hendisantika.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-users-login-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 22/10/21
+ * Time: 09.05
+ */
+@Entity
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue
+    private long id;
+    private @NotBlank String username;
+    private @NotBlank String password;
+    private @NotBlank boolean loggedIn;
+
+}
